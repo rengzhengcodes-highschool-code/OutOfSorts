ada9977352a9350e986f2cf29667754ab7d13cd9
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-16 13:23:51 -0500

    bubbleSort now breaks out of its infinite recursion loop.

--- Sorts.java
+++ Sorts.java
@@ -1,4 +1,21 @@
 public class Sorts{
+
+	public static boolean DEBUG = true;
+
+  public static void dprint(String s){
+    if(DEBUG){
+      System.out.println(s);
+    }
+  }
+
+	public static void main(String[]args){
+    //do this so you do not forget to turn it off
+    dprint("TURN THIS OFF BEFORE SUBMITTING YOUR CODE!!!!!!!!");
+
+    //Add the other parts of your main
+
+  }
+
   /**Bubble sort of an int array.
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
@@ -6,18 +23,24 @@ public class Sorts{
   public static void bubbleSort(int[] data) {
 		boolean sorted = false;
 
+		int pass = 0;
 		while (!sorted) {
-
-			for (int i = 0; i < (data.length - 1) - i; i++) {
+			boolean swapped = false;
+			for (int i = 0; i < (data.length - 1) - pass; i++) {
 				if (data[i] < data[i+1]) {
+					swapped = true;
 					int holder = data[i];
 					data[i] = data[i+1];
 					data[i+1] = holder;
 				}
 			}
 
+			if (!swapped) {
+				sorted = true;
+			}
+			pass++;
 		}
 
 	}
-
+	
 }
