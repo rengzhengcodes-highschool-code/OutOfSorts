568491def761869724bce9ab7b1d16f2bf6bc06e
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-22 12:46:36 -0500

    Added some whitespace for formatting.

--- Driver.java
+++ Driver.java
@@ -1,40 +1,40 @@
 import java.util.Arrays;
 public class Driver{
-  public static void main(String[]args){
+  public static void main(String[]args) {
     if(args.length < 2){
       System.out.println("usage: java Driver SIZE ALGO [STYLE]");
       System.out.println("supported ALGO: bubble,selection,insertion,test");
       System.out.println("supported STYLE: random,equal,sorted,reversed");
-    }else{
+    } else {
 
 
       int[] randData = new int[Integer.parseInt(args[0])];
 
       //overhead: 1 random array generation.
-      if(args.length < 3 || args[2].equals("random")){
+      if(args.length < 3 || args[2].equals("random")) {
 	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
-      }else if(args[2].equals("equal")){
+      } else if(args[2].equals("equal")) {
 	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
-      }else if(args[2].equals("sorted")){
+      } else if(args[2].equals("sorted")) {
 	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
-      }else if(args[2].equals("reversed")){
+      } else if(args[2].equals("reversed")) {
 	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
       }
 
-      if(args[1].equals("bubble")){
+      if(args[1].equals("bubble")) {
         Sorts.bubbleSort(randData);
       }
-      else if(args[1].equals("selection")){
+      else if(args[1].equals("selection")) {
         Sorts.selectionSort(randData);
       }
-      else if(args[1].equals("insertion")){
+      else if(args[1].equals("insertion")) {
         Sorts.insertionSort(randData);
       }
 
       //TEST MODE! This lets you see if your algorithm fails
       //for a particular type of array, or size of array.
       //This is not meant to be a timed test...
-      else if(args[1].equals("test")){
+      else if(args[1].equals("test")) {
 
         int[] randDataBubble = Arrays.copyOf(randData,randData.length);
         int[] randDataSelection = Arrays.copyOf(randData,randData.length);
@@ -44,21 +44,21 @@ public class Driver{
         Sorts.selectionSort(randDataSelection);
         Sorts.insertionSort(randDataInsertion);
 
-        if( Arrays.equals(randData,randDataBubble)){
+        if( Arrays.equals(randData,randDataBubble)) {
           System.out.println("Bubble Correct!");
-        }else{
+        } else {
          System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
         }
 
-        if( Arrays.equals(randData,randDataSelection)){
+        if( Arrays.equals(randData,randDataSelection)) {
           System.out.println("Selection Correct!");
         }else{
          System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
         }
 
-        if( Arrays.equals(randData,randDataInsertion)){
+        if( Arrays.equals(randData,randDataInsertion)) {
           System.out.println("Insertion Correct!");
-        }else{
+        } else {
          System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
         }
       }
