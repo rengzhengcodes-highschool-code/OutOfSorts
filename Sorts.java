8c0b068f88e2d0e3a830000e6490b56095f9eb54
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-22 13:27:26 -0500

    commented out dprint to avoid quadratic runtime.

--- Sorts.java
+++ Sorts.java
@@ -27,7 +27,7 @@ public class Sorts{
 
 		int pass = 0;
 		while (!sorted) {
-			dprint(Arrays.toString(data));
+			//dprint(Arrays.toString(data));
 
 			boolean swapped = false;
 			for (int i = 0; i < (data.length - 1) - pass; i++) {
@@ -49,7 +49,7 @@ public class Sorts{
 
 	public static void selectionSort(int[] data) {
 		for (int run = 0; run < data.length; run++) {
-			dprint(Arrays.toString(data));
+			//dprint(Arrays.toString(data));
 			int min = data[run];
 			int minIndex = run;
 
@@ -67,7 +67,7 @@ public class Sorts{
 	}
 
 	public static void insertionSort(int[] data) {
-		dprint(Arrays.toString(data));
+		//dprint(Arrays.toString(data));
 
 		for (int index = 1; index < data.length; index++) {
 
@@ -75,13 +75,14 @@ public class Sorts{
 			int value = data[index];
 
 			while (comparisonPosition >= 0 && data[comparisonPosition] > value) {
+				//dprint("triggered");
 				data[comparisonPosition+1] = data[comparisonPosition];//shifts previous position up if value is not supposed to go in this current position, as that means the value that was shifted to the previous value is larger than here so it must be inserted before this index.
 				comparisonPosition--;
 			}
 
 			data[comparisonPosition+1] = value;//+1 necessary as insertion position overshoots where it's supposed to terminate due to the way it evaluates and shifts.
 
-			dprint(Arrays.toString(data));
+			//dprint(Arrays.toString(data));
 		}
 
 	}
